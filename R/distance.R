857e5443e1cbf431f830bacd7e271442031806b2
Lupeng Zhang <90024939+lupengzhang@users.noreply.github.com>
2026-09-04 16:13:04 +0100

    Create distance.R

---
+++ R/distance.R
@@ -0,0 +1,15 @@
+#' Modified L1 Distance Matrix
+#'
+#' Computes the pairwise Manhattan distance divided by the data dimension.
+#'
+#' @param data A numeric matrix or data frame with observations in rows and
+#' variables in columns.
+#'
+#' @return A symmetric matrix of pairwise modified L1 distances.
+#' @export
+M.L1norm <- function(data) {
+p <- ncol(data)
+L1norm_matrix <- (1 / p) *
+as.matrix(stats::dist(data, method = "manhattan"))
+return(L1norm_matrix)
+}
